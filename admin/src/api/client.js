b4b655aa82904eb2c6ec16b97beeb10c54f504ae
Shrey Pandey <shreypandey419@gmail.com>
2026-07-28 10:30:58 +0530

    second commit

--- admin/src/api/client.js
+++ admin/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000",
+  baseURL: import.meta.env.VITE_API_URL || "https://sellbook.onrender.com",
 });
 
 apiClient.interceptors.request.use((config) => {
